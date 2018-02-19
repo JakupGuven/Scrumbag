5da8bc29abda990bab1466d7028f192bcca50e72
Jakup <jakup2105@gmail.com>
2018-02-19 15:42:37 +0100

    added Project skeleton logic and Task representation

--- Scrumbag/src/main/java/projectHandler/ProjectHandler.java
+++ Scrumbag/src/main/java/projectHandler/ProjectHandler.java
@@ -1,5 +1,80 @@
 package projectHandler;
 
+import java.util.ArrayList;
+
+import databaseManager.ProjectQuerier;
+
 public class ProjectHandler {
+	private ProjectQuerier projectQuerier;
+
+	public ProjectHandler(ProjectQuerier inProjectQuerier) {
+		projectQuerier = inProjectQuerier;
+	}
+
+	// FK1, FK7, FK8
+	public void createNewTask(int projectID, String taskName, String responsiblePerson, int taskPriority,
+			int taskStatus, int expectedTime) {
+
+	}
+
+	// FK2, FK4, FK7, FK8, FK21
+	public void editTask(int projectID, int taskID, String taskNAme, String responsiblePerson, int taskPriority,
+			int taskStatus, int expectedTime) {
+
+	}
+
+	// FK4
+	public void setTaskStatus(int projectID, int taskID, int taskStatus) {
+	}
+
+	// FK4, FK31
+	public void setTaskPriority(int projectID, int taskID, int taskPriority) {
+
+	}
+
+	// FK3
+	public void removeTask(int projectID, int taskID) {
+
+	}
+	//FK9
+	public void updateElapsedTime(int projectID, int taskID, int elapsedTime) {
+
+	}
+	//FK11
+	public void addUserToProject(int projectID, String username) {
+
+	}
+	//FK12
+	public void removeUserFromProject(int projectID, String username) {
+
+	}
+	//FK13
+	public void addMultipleUsersToProject(int projectID, ArrayList<String> userList) {
+
+	}
+	//FK14
+	public void removeMultipleUsersFromProject(int projectID, ArrayList<String> userList) {
+
+	}
+	//FK15,FK16,FK17,FK18
+	public ArrayList<Task> getRegisteredTasks(String username) {
+		ArrayList<Task> list = new ArrayList();
+		Task taskOne = new Task("create taskboard view", 10, 0, 0, "Mattias");
+		Task taskTwo = new Task("create taskboard logic", 15, 0, 0, "Mattias");
+		list.add(taskOne);
+		list.add(taskTwo);
+		return list;
+	}
+	//FK19, FK20
+	public ArrayList<Task> getSprintTasks(int projectID, int sprintID) {
+		ArrayList<Task> list = new ArrayList();
+		Task taskOne = new Task("create taskboard view", 10, 1, 0, "Mattias");
+		Task taskTwo = new Task("create taskboard logic", 15, 0, 0, "Mattias");
+		Task taskThree = new Task("create task statuses", 10, 1, 0, "Mattias");
+		list.add(taskOne);
+		list.add(taskTwo);
+		list.add(taskThree);
+		return list;
+	}
 
 }
--- Scrumbag/src/main/java/projectHandler/Task.java
+++ Scrumbag/src/main/java/projectHandler/Task.java
@@ -2,4 +2,83 @@ package projectHandler;
 
 public class Task {
 
+	private String taskName;
+
+	private User responsibleUser;
+
+	private int taskID;
+	private int expectedTime;
+	private int elapsedTime;
+	private int elapsedTimeHalfDays;
+	private int taskPriority;
+	private int taskStatus;
+	
+	public Task(String taskName, int expectedTime, int taskPriority, int taskStatus, String responsibleUser) {
+		
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public User getResponsibleUser() {
+		return responsibleUser;
+	}
+
+	public void setResponsibleUser(User responsibleUser) {
+		this.responsibleUser = responsibleUser;
+	}
+
+	public int getTaskID() {
+		return taskID;
+	}
+
+	private void setTaskID(int taskID) {
+		this.taskID = taskID;
+	}
+
+	public int getExpectedTime() {
+		return expectedTime;
+	}
+
+	public void setExpectedTime(int expectedTime) {
+		this.expectedTime = expectedTime;
+	}
+
+	public int getElapsedTime() {
+		return elapsedTime;
+	}
+
+	public void setElapsedTime(int elapsedTime) {
+		this.elapsedTime = elapsedTime;
+	}
+
+	public int getTaskPriority() {
+		return taskPriority;
+	}
+
+	public void setTaskPriority(int taskPriority) {
+		this.taskPriority = taskPriority;
+	}
+
+	public int getTaskStatus() {
+		return taskStatus;
+	}
+
+	public void setTaskStatus(int taskStatus) {
+		this.taskStatus = taskStatus;
+	}
+
+	public int getElapsedTimeHalfDays() {
+		return elapsedTimeHalfDays;
+	}
+
+	public void setElapsedTimeHalfDays(int elapsedTimeHalfDays) {
+		this.elapsedTimeHalfDays = elapsedTimeHalfDays;
+	}
+
 }
